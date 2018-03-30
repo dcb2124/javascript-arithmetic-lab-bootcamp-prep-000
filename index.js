0744df66d8ff66028816d128fd5bed4a35681419
DAVID BILLINGSLEY <dcb2124@gmail.com>
2018-03-30 01:32:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b){
+  return parseFloat(a) + parseFloat(b)
+}
+
+function subtract(a, b){
+  return (parseFloat(b) - parseFloat(a))
+}
+
+function multiply(a, b){
+  return (parseFloat(b) * parseFloat(a))
+}
+
+function divide(a, b){
+  return (parseFloat(a) - parseFloat(b))
+}
+
+function inc(n){
+  return ++n
+}
+
+function dec(n){
+  return --n
+}
+
+function makeInt(n){
+  return parseInt(n)
+}
